a4b1e4b97def7762fe326164e5881bb5f3c59f21
Alfian Andi Nugraha <alfian.23andi@gmail.com>
2021-11-30 09:33:58 +0700

    feat: add example of JSON Object

--- app/src/main/kotlin/kotlin_gson/app/App.kt
+++ app/src/main/kotlin/kotlin_gson/app/App.kt
@@ -3,12 +3,32 @@
  */
 package kotlin_gson.app
 
-import kotlin_gson.utilities.StringUtils
+import com.google.gson.Gson
 
-import org.apache.commons.text.WordUtils
+data class User (
+    val name: String = "",
+    val age: Int = 0
+)
 
 fun main() {
-    val tokens = StringUtils.split(MessageUtils.getMessage())
-    val result = StringUtils.join(tokens)
-    println(WordUtils.capitalize(result))
+    val user = User(
+        name = "Alfian",
+        age = 20
+    )
+    val userJson = Gson().toJson(user)
+
+    println("user :")
+    println(user)
+
+    println("===============")
+
+    println("userJson :")
+    println(userJson)
+    println("Is userJson string ? ${userJson is String}")
+
+    println("===============")
+
+    val convertedUserJson = Gson().fromJson<User>(userJson, User::class.java)
+    println("convertedUserJson :")
+    println(convertedUserJson)
 }
