825cc36cc14362911e3df6239f25063ceac34abb
Alfian Andi Nugraha <alfian.23andi@gmail.com>
2021-11-30 09:46:06 +0700

    feat: add example of JSON array

--- app/src/main/kotlin/kotlin_gson/app/App.kt
+++ app/src/main/kotlin/kotlin_gson/app/App.kt
@@ -31,4 +31,20 @@ fun main() {
     val convertedUserJson = Gson().fromJson<User>(userJson, User::class.java)
     println("convertedUserJson :")
     println(convertedUserJson)
+
+    println("===============")
+
+    val listUser = listOf(User("Andra", 20), User("Irvan", 19))
+    println("users :")
+    println(listUser)
+
+    println("===============")
+    val listUserJson = Gson().toJson(listUser)
+    println("usersJson :")
+    println(listUserJson)
+
+    println("===============")
+    val convertedListUserJson = Gson().fromJson(listUserJson, Array<User>::class.java).toList()
+    println("convertedListUserJson :")
+    println(convertedListUserJson)
 }
